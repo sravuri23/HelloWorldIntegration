cc69568041c5c3a2151d4d74d3f0f10d3328db7d
sravuri23 <sravuri23@gmail.com>
2017-09-21 10:49:49 +0100

    added new file

---
+++ selenium_practice/Integration_Practice_2.java
@@ -0,0 +1,12 @@
+package selenium_practice;
+
+public class Integration_Practice_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test created successfully");
+		
+	}
+
+}
